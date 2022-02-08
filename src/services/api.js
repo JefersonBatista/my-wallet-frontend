558403defef478c375e7668cbdf43162e741aac1
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-02-08 11:08:52 -0300

    Redirect API BASE_URL to deployed back-end

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://jeff-my-wallet.herokuapp.com/";
 
 function createAuth(token) {
   return { headers: { Authorization: `Bearer ${token}` } };
