c25c7ed0be9f7e914c0a7759a944df671ea31201
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-05-09 14:58:53 -0300

    Set API base URL to deployed back-end

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://jeff-my-wallet.herokuapp.com";
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://jeff-my-wallet.herokuapp.com",
 });
 
 function createAuth(token) {
